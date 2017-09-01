9dd08d89ab5a434b717f2a74ca3ee80a387cf80a
G2Bent <944921374@qq.com>
2017-09-01 08:53:23 +0800

    admin

---
+++ PollsWeb/Polls/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Question,Choice
+
+# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+class QuetionAdmin(admin.ModelAdmin):
+    filedsets = [
+        (None,{'fileds':['question_text']}),
+        ('Date information',{'filed':['pub_date'],'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text','pub_date')
+
+admin.site.register(Choice)
+admin.site.register(Question,QuetionAdmin)
